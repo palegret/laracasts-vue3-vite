0666ec45fb821ae66284d6b822a1d74e41f064b4
Phil Alegret <philip.alegret@gmail.com>
2022-11-14 06:45:34 -0500

    Added Counter view.

---
+++ src/views/CounterView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="counter">{{ count }}</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .counter {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
