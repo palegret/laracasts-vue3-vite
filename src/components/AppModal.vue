daea3fa761575926d18d3d5db44a5e71ce794dd1
Phil Alegret <philip.alegret@gmail.com>
2022-11-16 08:14:00 -0500

    Added modal transitions.

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -5,19 +5,64 @@ defineProps({
 </script>
 
 <template>
-  <div v-if="show" class="modal-mask">
-    <div class="modal-container">
-      <div>
-        <slot>default body</slot>
-      </div>
+  <!-- 
+  <Transition
+   STARTING POINT of an "entering" (initial) animation
+   enter-from-class="opacity-0 scale-125"
 
-      <footer class="modal-footer">
-        <slot name="footer">
-          <button @click="$emit('app-modal-close')">Close</button>
-        </slot>
-      </footer>
+   For the ENTIRE LENGTH of an "entering" animation
+   enter-active-class="transition duration-300"
+   
+   ENDING POINT of an "entering" (initial) animation
+   enter-to-class="opacity-100 scale-100"
+
+   STARTING POINT of a "leaving" (exiting/ending) animation
+   leave-from-class="opacity-100 scale-100"
+
+   For the ENTIRE LENGTH of an "leaving" (exiting/ending) animation
+   leave-active-class="transition duration-200"
+
+   ENDING POINT of a "leaving" (exiting/ending) animation
+   leave-to-class="opacity-0 scale-125"
+  > 
+
+  Alternate approach (uses Tailwind):
+
+    <Transition name="{transitionName}">
+  
+  Then, in stylesheet:
+
+    .{transitionName}-enter-from { @apply opacity-0 scale-125; }
+    .{transitionName}-enter-active { @apply transition duration-300; }
+    .{transitionName}-enter-to { @apply opacity-100 scale-100; }
+    .{transitionName}-leave-from { @apply opacity-100 scale-100; }
+    .{transitionName}-leave-active { @apply transition duration-200; }
+    .{transitionName}-leave-to { @apply opacity-0 scale-125; }
+
+  -->
+
+  <Transition
+    enter-from-class="opacity-0 scale-125"
+    enter-active-class="transition duration-300"
+    enter-to-class="opacity-100 scale-100"
+    leave-from-class="opacity-100 scale-100"
+    leave-active-class="transition duration-200"
+    leave-to-class="opacity-0 scale-125"
+  >
+    <div v-if="show" class="modal-mask">
+      <div class="modal-container">
+        <div>
+          <slot>default body</slot>
+        </div>
+
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="$emit('app-modal-close')">Close</button>
+          </slot>
+        </footer>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <style>
